947c4b9673da960df012f51d6fdff1018877f11c
Gordon Tyler <Gordon.Tyler@software.dell.com>
2016-11-14 15:44:05 -0500

    Server skeleton.

---
+++ server/src/main/java/org/teamhonda/trackapp/server/DispatchHandler.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 Quest Software, Inc.
+ * ALL RIGHTS RESERVED.
+ * 
+ * This software is the confidential and proprietary information of
+ * Quest Software Inc. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered
+ * into with Quest Software Inc.
+ * 
+ * QUEST SOFTWARE INC. MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT
+ * THE SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
+ * NON-INFRINGEMENT. QUEST SOFTWARE SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
+ * OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ */
+
+package org.teamhonda.trackapp.server;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.StatusCodes;
+
+/**
+ * @author Gordon Tyler
+ */
+public class DispatchHandler implements HttpHandler {
+
+private final HttpHandler mHandler;
+
+public DispatchHandler(HttpHandler handler) {
+    mHandler = handler;
+}
+
+@Override
+public void handleRequest(HttpServerExchange exchange) throws Exception {
+    if (exchange.isInIoThread()) {
+        exchange.dispatch(this);
+        return;
+    }
+
+    try {
+        mHandler.handleRequest(exchange);
+    }
+    catch (Exception e) {
+        exchange.setStatusCode(StatusCodes.INTERNAL_SERVER_ERROR);
+        exchange.getResponseSender().send(e.toString());
+    }
+}
+
+}
---
+++ server/src/main/java/org/teamhonda/trackapp/server/Main.java
@@ -0,0 +1,47 @@
+package org.teamhonda.trackapp.server;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import io.undertow.Handlers;
+import io.undertow.Undertow;
+import io.undertow.server.RoutingHandler;
+import org.bson.Document;
+import org.teamhonda.trackapp.server.json.Tracks;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+/**
+ * @author Gordon Tyler
+ */
+public class Main {
+
+public static void main(String[] args) {
+    final Gson gson = new GsonBuilder().create();
+    final MongoClient mongo = new MongoClient("192.168.99.100");
+    final MongoDatabase db = mongo.getDatabase("trackapp");
+
+    RoutingHandler rootHandler = Handlers.routing()
+            .get("/tracks", new DispatchHandler(exchange -> {
+                MongoCollection<Document> coll = db.getCollection("tracks");
+                ArrayList<Document> docs = coll.find().into(new ArrayList<>());
+                Tracks result = new Tracks(docs.stream()
+                        .map(d -> d.getString("name"))
+                        .collect(Collectors.toList()));
+
+                exchange.getResponseSender().send(gson.toJson(result));
+
+            }));
+
+    final Undertow server = Undertow.builder()
+            .addHttpListener(8080, "0.0.0.0", rootHandler)
+            .build();
+
+    server.start();
+}
+
+}
+
