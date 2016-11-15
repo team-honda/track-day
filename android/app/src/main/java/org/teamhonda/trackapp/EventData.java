48f54454e001508067d2eae3e4b759186515be12
David McAleer <david.mcaleer@hotmail.com>
2016-11-15 09:14:45 -0500

    EventData record

---
+++ android/app/src/main/java/org/teamhonda/trackapp/EventData.java
@@ -0,0 +1,19 @@
+package org.teamhonda.trackapp;
+
+import java.util.Date;
+
+public class EventData {
+    private Date date;
+    private String track;
+    private String layout;
+    private String layoutMapUrl;
+    private double latitude;
+    private double longitude;
+    private String organizer;
+    private String organizerWebsiteUrl;
+    private String preRegCutoffDate;
+    private double preRegCost;
+    private double normalCost;
+    private boolean registered;
+    private String notes;
+}
