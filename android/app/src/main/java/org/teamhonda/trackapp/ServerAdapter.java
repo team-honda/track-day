77bc55c4a8c4769dac62c37924ac6aadc3259716
David McAleer <david.mcaleer@hotmail.com>
2016-11-15 09:36:02 -0500

    Server/Calendar adapter stubs

---
+++ android/app/src/main/java/org/teamhonda/trackapp/ServerAdapter.java
@@ -0,0 +1,13 @@
+package org.teamhonda.trackapp;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+public class ServerAdapter {
+
+    public List<EventData> getEventData(Date from, Date to) {
+        return Collections.emptyList();
+    }
+
+}
